155514c61c372a6788812b757601c77297e7a02c
DeokHaengCho <stargazer61@naver.com>
2020-11-05 23:26:13 +0900

    2020.11.05 WordRelay

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,13 +1,34 @@
 const React = require('react')
-const {Component} = React
+const {useState} = React
 
-class WordRelay extends Component{
-    state={
-        text:'hello webpack'
+const WordRelay = () => {
+    const [firstWord, setFirstWord] = useState("가나다라마바사".charAt(Math.floor(Math.random()*6)))
+    const [secondWord, setSecondWord] = useState('')
+    const [bingo, setBingo] = useState(false)
+
+    const onChangeHandler = e => {
+        setSecondWord(e.currentTarget.value)
     }
-    render(){
-        return <h1>{this.state.text}</h1>
+
+    const onClickHandler = () => {
+        if(firstWord.charAt(firstWord.length-1)===secondWord.charAt(0)){
+            setFirstWord(secondWord)
+            setSecondWord('')
+            setBingo(true)
+        }else{
+            setSecondWord('')
+            setBingo(false)
+        }
     }
+    return (
+        <>
+            <h1>{firstWord}</h1>
+            <input type="text" onChange={onChangeHandler} value={secondWord}/>
+            <button onClick={onClickHandler}>제출</button>
+            <br/>
+            <div>{bingo? "정답": "오답"}</div>
+        </>
+    )
 }
 
 module.exports = WordRelay
