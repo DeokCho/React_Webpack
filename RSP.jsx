1502aeb71e9d46f90b0b1c29f1cfa6fad1f461b6
DeokHaengCho <stargazer61@naver.com>
2020-11-11 20:49:52 +0900

    2020.11.11 빼빼로데이

--- RSP.jsx
+++ RSP.jsx
@@ -1,36 +1,70 @@
 import React, {useState,useEffect} from 'react'
 
+const scores = {
+    sicssors: 1,
+    rock:0,
+    paper:-1
+}
+
 const RSP = () => {
     const [result, setResult]=useState('')
     const [imgCoord, setImgCoord]=useState(0)
-    const [score, setScore]=useState(0)
+    const [score, setscore]=useState(0)
 
     const rspCoords = {
         rock:0,
-        scissor:'-142px',
+        sicssors:'-142px',
         paper:'-284px'
     }
+    
+    let interval
 
     useEffect(()=>{
-        let interval
-        interval = setInterval(()=>{
-            if(imgCoord===rspCoords.rock){
-                setImgCoord(rspCoords.scissor) 
-
-            }else if(imgCoord===rspCoords.scissor){
-                setImgCoord(rspCoords.paper) 
-
-            }else if(imgCoord===rspCoords.paper){
-                setImgCoord(rspCoords.rock) 
-                
-            }
-            clearInterval(interval)
-        }, 1000)    
+        interval = setInterval(changeHand, 300)    
     },[imgCoord])
 
-    const onClickBtn = (choice) => {
-        if(choice===imgCoord){
+    const computerChoice = (imgCoord => {
+        return Object.entries(rspCoords).find(function(v){
+            return v[1] === imgCoord
+        })[0]
+    })
+
+    const changeHand = () => {
+        if(imgCoord===rspCoords.rock){
+            setImgCoord(rspCoords.sicssors) 
+
+        }else if(imgCoord===rspCoords.sicssors){
+            setImgCoord(rspCoords.paper) 
+
+        }else if(imgCoord===rspCoords.paper){
+            setImgCoord(rspCoords.rock) 
+            
         }
+        clearInterval(interval)
+    }
+
+    const onClickBtn = (choice) => {
+        clearInterval(interval)
+        setTimeout(()=>{
+            interval = setInterval(changeHand, 300) 
+        },2000)
+        const myScore = scores[choice]
+        const cpuScore = scores[computerChoice(imgCoord)]
+        const diff = myScore - cpuScore
+        if(diff === 0){
+            setResult('비겼습니다.')
+        }else if([-1, 2].includes(diff)){
+            setResult('이겼습니다.')
+            setscore((prevScore)=>{
+                return prevScore +1
+            })
+        }else {
+            setResult('졌습니다.')
+            setscore((prevScore)=>{
+                return prevScore -1
+            })
+        }
+         
     }
 
 
@@ -38,7 +72,7 @@ const RSP = () => {
         <>
             <div id="computer" style={{background: `url(./23182267.jpg) ${imgCoord} 0`}}/>
             <button onClick={()=>{onClickBtn('rock')}}>주먹</button>
-            <button onClick={()=>{onClickBtn('sicssor')}}>가위</button>
+            <button onClick={()=>{onClickBtn('sicssors')}}>가위</button>
             <button onClick={()=>{onClickBtn('paper')}}>보</button>
             <br/>
             <div>현재 : {score}점</div>
